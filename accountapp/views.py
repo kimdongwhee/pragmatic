bfe11013fd4d4371ab29fc878fb998778046d7de
kimdongwhee <romangrism@gmail.com>
2024-07-05 10:50:24 +0900

    make new templates

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,4 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello_world(request):
-    return HttpResponse("Hello world")
+    return render(request, "base.html")
+    #render 인자로 request와 템플릿 파일 적용 ↑
+    # return HttpResponse("Hello world")

ee0ad08e2fbdcb6bc96af0231bce7ca8d334f949
kimdongwhee <romangrism@gmail.com>
2024-07-05 18:57:35 +0900

    django get post

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello_world(request):
-    return render(request, "accountapp/helloworlds.html")
-    #render 인자로 request와 템플릿 파일 적용 ↑
-    # return HttpResponse("Hello world")
+    if request.method == "POST":
+        #POST 방식으로 요청시 accountapp/hello_world.html 페이지로 전환 및 post method가 담긴 text를 context라는 꾸러미에 담아 함께 반환
+        return render(request, "accountapp/hello_world.html", context={'text':'POST METHOD!'})
+    else:
+        #GET 방식으로 요청시 accountapp/hello_world.html 페이지로 전환 및 post method가 담긴 text를 context라는 꾸러미에 담아 함께 반환
+        return render(request, "accountapp/hello_world.html", context={'text': 'GET METHOD!'})
